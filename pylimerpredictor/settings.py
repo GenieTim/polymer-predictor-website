79f6fa226596449d2ec9bf69994b59b59fd2313a
Tim Bernhard <tim@bernhard-webstudio.ch>
2025-06-20 09:50:51 +0200

    In production, use postgresql

--- pylimerpredictor/settings.py
+++ pylimerpredictor/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"].lower() == "true"
+PRODUCTION = os.environ["PRODUCTION"].lower() == "true"
 
 ALLOWED_HOSTS = [
     "www.polymer-predictor.ethz.ch",
@@ -80,10 +81,21 @@ WSGI_APPLICATION = "pylimerpredictor.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": (
+        {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+        if not PRODUCTION
+        else {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST", "localhost"),
+            "PORT": os.environ.get("DB_PORT", "5432"),
+        }
+    )
 }
 
 
@@ -116,6 +128,15 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
+
+if PRODUCTION:
+    # Use a secure session cookie in production
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = "DENY"
 
 
 # Static files (CSS, JavaScript, Images)
