c9223a4851a808fe643d4ae04e50da3122388867
Tim Bernhard <tim@bernhard-webstudio.ch>
2025-06-20 09:53:04 +0200

    Use defaults for debug & prod settings

--- pylimerpredictor/settings.py
+++ pylimerpredictor/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("The SECRET_KEY environment variable is not set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"].lower() == "true"
-PRODUCTION = os.environ["PRODUCTION"].lower() == "true"
+DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
+PRODUCTION = os.environ.get("PRODUCTION", "true").lower() == "true"
 
 ALLOWED_HOSTS = [
     "www.polymer-predictor.ethz.ch",
